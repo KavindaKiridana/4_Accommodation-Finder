aaffe5e36cb4f8f00c9e4a97233d234d475053e0
Kavinda Kiridana <kavindakiridena@gmail.com>
2025-01-14 21:43:30 +0530

    moved seeUser.php file to root folder

--- seeUser.php
+++ seeUser.php
@@ -1,6 +1,6 @@
 <?php 
-include('../header.php');
-require '../config.php';
+include('header.php');
+require 'config.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
@@ -93,4 +93,4 @@ if (!isset($_SESSION['user_id'])) {
     }
 </style>
 
-<?php include('../footer.php'); ?>
+<?php include('footer.php'); ?>
